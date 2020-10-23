ff60f273754cde5874558d53cc0d329cf6512e75
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-22 23:53:10 -0400

    added code for roller view model

--- app/src/main/java/sheridan/kananid/assignment2/ui/roller/RollerViewModel.kt
+++ app/src/main/java/sheridan/kananid/assignment2/ui/roller/RollerViewModel.kt
@@ -1,2 +1,35 @@
 package sheridan.kananid.assignment2.ui.roller
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import sheridan.kananid.assignment2.database.Envelope
+import sheridan.kananid.assignment2.database.EnvelopeDao
+import sheridan.kananid.assignment2.database.EnvelopeDatabase
+
+
+class RollerViewModel(application: Application) : AndroidViewModel(application) {
+    // TODO: Implement the ViewModel
+
+    private val _envelopeId = MutableLiveData<Long?>().apply{
+        value = null
+    }
+
+    val envelopeId: LiveData<Long?> = _envelopeId
+
+    private val envelopeDao: EnvelopeDao =
+        EnvelopeDatabase.getInstance(application).envelopeDao
+
+    fun send(envelope: Envelope){
+        viewModelScope.launch {
+            _envelopeId.value = envelopeDao.insert(envelope)
+        }
+    }
+
+    fun reset(){
+        _envelopeId.value = null
+    }
+}
