8cb3f2bb7228d780665009f1922ff89a61d6c54e
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-22 23:52:23 -0400

    added code for history view model

--- app/src/main/java/sheridan/kananid/assignment2/ui/history/HistoryViewModel.kt
+++ app/src/main/java/sheridan/kananid/assignment2/ui/history/HistoryViewModel.kt
@@ -1,2 +1,23 @@
 package sheridan.kananid.assignment2.ui.history
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import sheridan.kananid.assignment2.database.Envelope
+import sheridan.kananid.assignment2.database.EnvelopeDao
+import sheridan.kananid.assignment2.database.EnvelopeDatabase
+
+class HistoryViewModel(application: Application): AndroidViewModel(application){
+    private val envelopeDao: EnvelopeDao =
+        EnvelopeDatabase.getInstance(application).envelopeDao
+
+    val history: LiveData<List<Envelope>> = envelopeDao.getAll()
+
+    fun clear(){
+        viewModelScope.launch {
+            envelopeDao.deleteAll()
+        }
+    }
+}
