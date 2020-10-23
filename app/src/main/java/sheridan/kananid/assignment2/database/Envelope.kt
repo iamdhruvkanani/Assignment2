7ca8851cceb62fcf8eb7d17368231bee520f53c6
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-22 23:50:14 -0400

    added code for envelope

--- app/src/main/java/sheridan/kananid/assignment2/database/Envelope.kt
+++ app/src/main/java/sheridan/kananid/assignment2/database/Envelope.kt
@@ -1,2 +1,23 @@
 package sheridan.kananid.assignment2.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "gamescores")
+data class Envelope(
+    @PrimaryKey(autoGenerate = true)
+    var id : Long,
+
+    @ColumnInfo(name = "die_one")
+    val dieOne : String,
+    @ColumnInfo(name = "die_two")
+    val dieTwo : String,
+    @ColumnInfo(name = "die_three")
+    val dieThree : String,
+    @ColumnInfo(name = "die_total")
+    val dieTotal : String,
+    @ColumnInfo(name = "time_stamp")
+    val timeStamp: Date
+)
