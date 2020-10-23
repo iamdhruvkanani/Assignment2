d6d48cf7b538078a499ce285a1d894479aca129d
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-22 23:50:30 -0400

    added code for envelopeDao

--- app/src/main/java/sheridan/kananid/assignment2/database/EnvelopeDao.kt
+++ app/src/main/java/sheridan/kananid/assignment2/database/EnvelopeDao.kt
@@ -1,4 +1,22 @@
 package sheridan.kananid.assignment2.database
 
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
 interface EnvelopeDao {
+    @Insert
+    suspend fun insert(envelope: Envelope): Long
+
+    @Query("SELECT * FROM gamescores ORDER BY id")
+    fun getAll() : LiveData<List<Envelope>>
+
+    @Delete
+    suspend fun delete(envelope: Envelope)
+
+    @Query("DELETE FROM gamescores")
+    suspend fun deleteAll()
 }
