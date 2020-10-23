101f70a7f04da62bac0fc19c8d8d77e7346df1f9
Dhruv Kanani <kananid@sheridancollege.ca>
2020-10-22 23:50:47 -0400

    added code for envelopeDatabase

--- app/src/main/java/sheridan/kananid/assignment2/database/EnvelopeDatabase.kt
+++ app/src/main/java/sheridan/kananid/assignment2/database/EnvelopeDatabase.kt
@@ -1,2 +1,39 @@
 package sheridan.kananid.assignment2.database
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(entities = [Envelope::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
+abstract class EnvelopeDatabase: RoomDatabase(){
+
+    abstract val envelopeDao: EnvelopeDao
+
+    companion object{
+
+        @Volatile
+        private var INSTANCE: EnvelopeDatabase? = null
+
+        fun getInstance(context: Context): EnvelopeDatabase {
+
+            synchronized(this) {
+                var instance = INSTANCE
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        EnvelopeDatabase::class.java,
+                        "envelope_database"
+                    ).fallbackToDestructiveMigration().build()
+
+                    INSTANCE = instance
+                }
+
+                return instance
+            }
+        }
+    }
+
+}
